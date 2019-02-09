ac4c801df2b799c49df783748a5919b16af94d38
Lucas Gertel <lgertel@gmail.com>
2019-02-09 13:57:27 -0200

    Report restaurant unavailabilites

---
+++ scheduler/src/main/java/br/com/grts/controller/RestaurantController.java
@@ -0,0 +1,25 @@
+package br.com.grts.controller;
+
+import br.com.grts.model.Unavailability;
+import br.com.grts.repository.RestaurantRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/restaurants")
+@AllArgsConstructor
+public class RestaurantController {
+
+  private final RestaurantRepository restaurantRepository;
+
+  @GetMapping("/unavailabilities/{axonId}")
+  public Set<Unavailability> findByUnavailabilitiesByRestaurant(@PathVariable String axonId) {
+    return restaurantRepository.findByAxonId(axonId).getUnavailabilities();
+  }
+}
