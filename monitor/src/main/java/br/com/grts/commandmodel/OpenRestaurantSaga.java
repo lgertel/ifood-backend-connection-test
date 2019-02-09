a916907cf869f3ba814b7b231de715fae7627db1
Lucas Gertel <lgertel@gmail.com>
2019-02-09 18:28:51 -0200

    Saga not working :( not much time left, unfortunatelly.

--- monitor/src/main/java/br/com/grts/commandmodel/OpenRestaurantSaga.java
+++ monitor/src/main/java/br/com/grts/commandmodel/OpenRestaurantSaga.java
@@ -1,41 +1,36 @@
 package br.com.grts.commandmodel;
 
 import br.com.grts.coreapi.RestaurantToggledOfflineEvent;
-import br.com.grts.coreapi.RestaurantToggledOfflineSagaEvent;
 import br.com.grts.coreapi.RestaurantToggledOnlineEvent;
 import br.com.grts.coreapi.ToggleRestaurantOfflineCommand;
-import lombok.AllArgsConstructor;
 import lombok.Setter;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.commandhandling.gateway.CommandGateway;
-import org.axonframework.deadline.DeadlineManager;
-import org.axonframework.eventhandling.EventHandler;
-import org.axonframework.eventhandling.saga.EndSaga;
 import org.axonframework.eventhandling.saga.SagaEventHandler;
 import org.axonframework.eventhandling.saga.StartSaga;
 import org.axonframework.eventhandling.scheduling.EventScheduler;
-import org.axonframework.eventhandling.scheduling.java.SimpleEventScheduler;
 import org.axonframework.spring.stereotype.Saga;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.Serializable;
-import java.time.Duration;
+import java.time.Instant;
 
 @Saga
 @Slf4j
 public class OpenRestaurantSaga implements Serializable {
 
   private static final Logger logger = LoggerFactory.getLogger(OpenRestaurantSaga.class);
+  String restaurantId;
 
   @Autowired @Setter
   private transient CommandGateway commandGateway;
 
   @Autowired @Setter
-  transient SimpleEventScheduler eventScheduler;
-
-  String restaurantId;
+  private transient EventScheduler eventScheduler;
 
   @StartSaga
   @SagaEventHandler(associationProperty = "restaurantId")
@@ -43,13 +38,16 @@ public class OpenRestaurantSaga implements Serializable {
     log.info("Handling {} saga: {}", event.getClass().getSimpleName(), event);
     restaurantId = event.getRestaurantId();
 
-    commandGateway.send(new ToggleRestaurantOfflineCommand(restaurantId));
-    eventScheduler.schedule(Duration.ofSeconds(10), new RestaurantToggledOfflineEvent(restaurantId));
+//    commandGateway.send(new ToggleRestaurantOfflineCommand(restaurantId));
+
+    eventScheduler.schedule(
+        Instant.now().plusSeconds(10),
+        new RestaurantToggledOfflineEvent(event.getRestaurantId())
+    );
   }
 
-  @EndSaga
   @SagaEventHandler(associationProperty = "restaurantId")
-  public void handle(RestaurantToggledOfflineEvent event) {
-    log.info("Ending {} saga: {}", event.getClass().getSimpleName(), event);
+  public void on(RestaurantToggledOfflineEvent event) {
+    log.info("Handling {} scheduled event: {}", event.getClass().getSimpleName(), event);
   }
 }
--- monitor/src/main/java/br/com/grts/config/AxonConfiguration.java
+++ monitor/src/main/java/br/com/grts/config/AxonConfiguration.java
@@ -1,14 +1,24 @@
 package br.com.grts.config;
 
-import org.axonframework.spring.eventhandling.scheduling.java.SimpleEventSchedulerFactoryBean;
+import org.axonframework.eventhandling.EventBus;
+import org.axonframework.eventhandling.scheduling.EventScheduler;
+import org.axonframework.eventhandling.scheduling.java.SimpleEventScheduler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executors;
 
 @Configuration
 public class AxonConfiguration {
 
+  @Autowired
+  private AxonConfiguration axonConfiguration;
+  @Autowired
+  private EventBus eventBus;
+
   @Bean
-  public SimpleEventSchedulerFactoryBean simpleEventSchedulerFactoryBean() {
-    return new SimpleEventSchedulerFactoryBean();
+  public EventScheduler eventScheduler() {
+    return new SimpleEventScheduler(Executors.newScheduledThreadPool(5), eventBus);
   }
 }
